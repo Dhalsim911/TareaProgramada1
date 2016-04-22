e03caef87cb5ee959f72471b6bbcc5b0feafb1aa
Pablo <pabloj1092@gmail.com>
2016-04-21 18:03:08 -0600

    little fix in import

--- run_cache.py
+++ run_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-from lib import cache
+from lib.cache import cache
 
 #check the integrity of the arguments
 if (len(sys.argv) < 4):
@@ -11,5 +11,5 @@ asoc = sys.argv[1]
 c_size = sys.argv[2]
 b_size = sys.argv[3]
 
-(hits, miss) = cache.cache(asoc, c_size, b_size)
+(hits, miss) = cache(asoc, c_size, b_size)
 print "misses: ",miss, "hits: ", hits
